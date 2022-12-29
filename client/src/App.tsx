b20b75846cfe571cc008d10395f01f4437cb07d0
Zak Mayfield <zakmayfield@gmail.com>
2022-12-29 11:58:26 -0500

    added app code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { gql, useQuery } from '@apollo/client';
+import { Product } from './gql/graphql';
+
+const GET_PRODUCTS = gql`
+  query GetAllProducts {
+    getAllProducts {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const { loading, data } = useQuery(GET_PRODUCTS);
+
+  useEffect(() => {
+    if (data?.getAllProducts.length !== 0) {
+      setProducts(data?.getAllProducts);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    console.log('products', products);
+  }, [products]);
+
+  if (loading) return <div> loading... </div>;
+
   return (
-    <div></div>
+    <div className='App'>
+      <header className='App-header'></header>
+    </div>
   );
 }
 
